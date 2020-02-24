0dcc2b038648e29eaebfccdb2613cb3cb5841441
Yuko Uda <yuko.uda0110@gmail.com>
2020-02-23 23:23:16 -0800

    Create setUpPage function and also start writing logic for current word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,7 +10,7 @@ const wordGuessGame = {
   isStarted: false,
   gameNum: 0,
   wins: 0,
-  numGuessRemain: 12,
+  guessesRemainNum: 12,
   lettersGuessed: [],
   words: [
     {
@@ -35,6 +35,62 @@ const wordGuessGame = {
   },
   addWin: function () {
     this.wins += 1;
-  }
+  },
+  writeWinsNum: function () {
+    htmlEl.winsNum.textContent= this.wins;
+  },
+  // writeHint: function () {
+  //   htmlEl.hint.textContent= this.wins;
+  // },
+  writeGuessRemainNum: function () {
+    htmlEl.guessesRemainNum.textContent= this.guessesRemainNum;
+  },
 };
+
+function setUpPage() {
+  wordGuessGame.writeWinsNum();
+  wordGuessGame.writeGuessRemainNum();
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  setUpPage();
+});
+
+const hiddenLetter = '_'
+const currentWord = wordGuessGame.words[wordGuessGame.gameNum].word.toLowerCase();
+let displayedWord = '';
+let updatedWord = '';
+
+document.onkeyup = function(e) {
+  if (!wordGuessGame.isStarted) {
+    console.log('game is not started!')
+    wordGuessGame.isStarted = true;
+    console.log(wordGuessGame.isStarted);
+    displayedWord = hiddenLetter.repeat(currentWord.length);
+    htmlEl.wordDisplay.textContent = displayedWord;
+
+    htmlEl.hint.textContent = wordGuessGame.words[wordGuessGame.gameNum].hint1;
+  }
+
+  const letter = e.key.toLowerCase();
+
+  for (let i = 0; i < currentWord.length; i++) {
+    if (currentWord[i] === letter) {
+      updatedWord += letter;
+      console.log('1: ' + updatedWord);
+    } else if (displayedWord[i] !== hiddenLetter) {
+      updatedWord += currentWord[i];
+      console.log('2: ' + updatedWord);
+    } else {
+      updatedWord += hiddenLetter;
+      console.log('3: ' + updatedWord);
+    }
+  }
+  htmlEl.wordDisplay.innerHTML = updatedWord;
+
+
+};
+
+
+
 
