445e7f03cf388cf50b070c819ab10d2b0fc3d19f
Yuko Uda <yuko.uda0110@gmail.com>
2020-02-28 13:53:31 -0800

    Create a flag to prevent the key event from happening while waiting for the next word to start after winning / losing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -84,6 +84,10 @@ const hiddenLetter = '_';
 let currentWordArray = wordGuessGame.words[wordGuessGame.gameNum].word.toLowerCase().split('');
 let displayedWordArray = [];
 
+// Flag to prevent the key event from happening while waiting for the next word to start after winning / losing.
+// 'wait' must start as false otherwise the game will never start!!
+let wait = false;
+
 function getCurrentWordArray() {
   console.log('getCurrentWordArray function is called! index: ' + wordGuessGame.gameNum);
   currentWordArray = wordGuessGame.words[wordGuessGame.gameNum].word.toLowerCase().split('');
@@ -92,6 +96,7 @@ function getCurrentWordArray() {
 function startNewWord() {
   console.log('startNewWord!!!');
 
+  wait = false;
   wordGuessGame.resetLettersGuessed();
   htmlEl.guessedLetters.textContent = wordGuessGame.lettersGuessed;
 
@@ -113,6 +118,10 @@ function startNewWord() {
 }
 
 document.onkeydown = function(e) {
+  if (wait) {
+    return;
+  }
+
   const userGuess = e.key;
 
   // If the game is already started &&
@@ -132,33 +141,14 @@ document.onkeydown = function(e) {
       console.log(displayedWord);
       htmlEl.wordDisplay.textContent = displayedWord;
 
-      // *************** The player succeed to guess the word correctly.
-      if (displayedWord === wordGuessGame.words[wordGuessGame.gameNum].word.toLowerCase()) {
-        wordGuessGame.addWin();
-        htmlEl.winsNum.textContent = wordGuessGame.wins;
-        htmlEl.direction.textContent = directions.win;
-        wordGuessGame.addGameNum();
-        window.setTimeout(startNewWord, 3000);
-      }  // *************
-
-      // Reduce "Number of Guesses Remaining" until it hits 0.
-      if (wordGuessGame.guessesRemainNum > 1) {
-        console.log('userGuess: ' + userGuess);
-        console.log('index: ' + wordGuessGame.lettersGuessed.indexOf(userGuess));
+      // Reduce "Number of Guesses Remaining" until it hits 0 if the displayedWord is not the same as currentWord.
+      if (wordGuessGame.guessesRemainNum > 0) {
         if (wordGuessGame.lettersGuessed.indexOf(userGuess) < 0) {
           wordGuessGame.guessesRemainNum -= 1;
           htmlEl.guessesRemainNum.textContent = wordGuessGame.guessesRemainNum;
         } else {
           htmlEl.guessesRemainNum.textContent = wordGuessGame.guessesRemainNum;
         }
-      } else {  // *************** The player failed to guess the word. ("Number of Guesses Remaining" hits 0)
-        htmlEl.guessesRemainNum.textContent = '0';
-        htmlEl.direction.textContent = directions.fail;
-        if (wordGuessGame.gameNum < wordGuessGame.words.length) {
-          wordGuessGame.addGameNum();
-          console.log('wordGuessGame.gameNum: ' + wordGuessGame.gameNum);
-          window.setTimeout(startNewWord, 3000);
-        }  // ********************
       }
 
       // Only if the userGuess is not included in the lettersGuessed array, add it to the array.
@@ -169,6 +159,28 @@ document.onkeydown = function(e) {
           html += `<span class="guessed--letter">${letter}</span>`;
         })
         htmlEl.guessedLetters.innerHTML = html;
+      }
+
+      // *************** WIN!!! The player succeed to guess the word correctly.
+      if (displayedWord === wordGuessGame.words[wordGuessGame.gameNum].word.toLowerCase()) {
+        wait = true;
+        wordGuessGame.addWin();
+        htmlEl.winsNum.textContent = wordGuessGame.wins;
+        htmlEl.direction.textContent = directions.win;
+        wordGuessGame.addGameNum();
+        window.setTimeout(startNewWord, 3000);
+      }  // *************
+
+      // *************** LOSE!!! The player failed to guess the word. ("Number of Guesses Remaining" hits 0)
+      if (wordGuessGame.guessesRemainNum <= 0) {
+        wait = true;
+        htmlEl.guessesRemainNum.textContent = '0';
+        htmlEl.direction.textContent = directions.fail;
+        if (wordGuessGame.gameNum < wordGuessGame.words.length) {
+          wordGuessGame.addGameNum();
+          console.log('wordGuessGame.gameNum: ' + wordGuessGame.gameNum);
+          window.setTimeout(startNewWord, 3000);
+        }  // ********************
       }
 
     } else { // If the userGuess is NOT an alphabet
